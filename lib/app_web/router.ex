6cba960da21f195528ebfc38515e292ebb12d27c
nelsonic <nelson+github@dwyl.io>
2020-05-19 13:16:42 +0100

    use Content plug in router.ex

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -3,20 +3,25 @@ defmodule AppWeb.Router do
 
   pipeline :any do
     plug :accepts, ~w(html json)
-    plug :negotiate
+    plug Content, %{html_plugs: [
+      &fetch_session/2,
+      &fetch_flash/2,
+      &protect_from_forgery/2,
+      &put_secure_browser_headers/2
+    ]}
   end
 
-  defp negotiate(conn, []) do
-    if AppWeb.QuotesController.get_accept_header(conn) =~ "json" do
-      conn
-    else
-      conn
-      |> fetch_session([])
-      |> fetch_flash([])
-      |> protect_from_forgery([])
-      |> put_secure_browser_headers([])
-    end
-  end
+  # defp negotiate(conn, []) do
+  #   if AppWeb.QuotesController.get_accept_header(conn) =~ "json" do
+  #     conn
+  #   else
+  #     conn
+  #     |> ([])
+  #     |> fetch_flash([])
+  #     |> protect_from_forgery([])
+  #     |> put_secure_browser_headers([])
+  #   end
+  # end
 
   scope "/", AppWeb do
     pipe_through :any
