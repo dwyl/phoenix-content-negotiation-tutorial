3e4f49da2e6b700bdaee5ebe40f15298bc0da3df
nelsonic <nelson+github@dwyl.io>
2020-05-19 13:42:03 +0100

    use Content.reply/5 in QuotesController.index/2 to simplify!

--- lib/app_web/controllers/quotes_controller.ex
+++ lib/app_web/controllers/quotes_controller.ex
@@ -10,24 +10,6 @@ defmodule AppWeb.QuotesController do
   def index(conn, _params) do
     q = Quotes.random() |> transform_string_keys_to_atoms
 
-    if get_accept_header(conn) =~ "json" do
-      json(conn, q)
-    else
-      render(conn, "index.html", quote: q)
-    end
-  end
-
-  @doc """
-  `get_accept_header/2` gets the "accept" header from req_headers.
-  Defaults to "text/html" if no header is set.
-  """
-  def get_accept_header(conn) do
-    case List.keyfind(conn.req_headers, "accept", 0) do
-      {"accept", accept} ->
-        accept
-
-      nil ->
-        "tex/html"
-    end
+    Content.reply(conn, &render/3, "index.html", &json/2, q)
   end
 end
